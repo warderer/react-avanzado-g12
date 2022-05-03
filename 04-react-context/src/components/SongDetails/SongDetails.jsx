d80e736a3cbac10bbaa5fa955742ce979f60e9ab
César Guerra <warderer@gmail.com>
2022-05-02 21:48:32 -0500

    💄 Imagen y Arreglos menores de UI

--- 04-react-context/src/components/SongDetails/SongDetails.jsx
+++ 04-react-context/src/components/SongDetails/SongDetails.jsx
@@ -6,10 +6,11 @@ const SongDetails = () => {
     <>
       {context.selectedSong.title
         ? <div>
+          <img src={`https://picsum.photos/seed/${context.selectedSong.id}/400/400`} alt='Portada del Disco' />
           <h1>{context.selectedSong.title}</h1>
           <h4>{context.selectedSong.artist}</h4>
           <h6>{context.selectedSong.year}</h6>
-        </div>
+          </div>
         : <h1>Selecciona una canción</h1>}
     </>
   )
