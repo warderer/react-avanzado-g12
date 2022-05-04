d2862e2f422de395b32036fd1235669959dfd604
César Guerra <warderer@gmail.com>
2022-05-03 21:03:39 -0500

    🚧 Estructura de archivos y carpetas del proyecto

--- 05-react-auth/src/App.jsx
+++ 05-react-auth/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import { Navbar } from '@/components/Navbar'
+import { Home, Login, Signup, Secret } from '@/pages'
 
 function App () {
   return (
     <div>
+      <Navbar />
       <h1>App</h1>
+      <Home />
+      <Login />
+      <Signup />
+      <Secret />
     </div>
   )
 }
---
+++ 05-react-auth/src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from './Home'
+import Login from './Login'
+import Signup from './Signup'
+import Secret from './Secret'
+
+export {
+  Home,
+  Login,
+  Secret,
+  Signup
+}
