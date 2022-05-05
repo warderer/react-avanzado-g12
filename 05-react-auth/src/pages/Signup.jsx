3aad91edd2a0ce9802417f0843e75e81afefceb5
César Guerra <warderer@gmail.com>
2022-05-04 20:27:30 -0500

    💄 UI Signup

--- 05-react-auth/src/pages/Signup.jsx
+++ 05-react-auth/src/pages/Signup.jsx
@@ -1,6 +1,98 @@
+import '@/assets/css/form.css'
+import logo from '@/logo.svg'
+
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <main className='form-signin'>
+      <form onSubmit={() => {}}>
+        <img className='mb-4' src={logo} alt='' width='72' height='57' />
+        <h1 className='h3 mb-3 fw-normal'>Please sign up</h1>
+
+        <div className='form-floating my-1'>
+          <input
+            type='text'
+            className='form-control'
+            id='first_name'
+            name='first_name'
+            placeholder='John'
+            value=''
+            onChange={() => {}}
+          />
+          <label htmlFor='first_name'>First name</label>
+        </div>
+
+        <div className='form-floating my-1'>
+          <input
+            type='text'
+            className='form-control'
+            id='last_name'
+            name='last_name'
+            placeholder='Doe'
+            value=''
+            onChange={() => {}}
+          />
+          <label htmlFor='last_name'>Last name</label>
+        </div>
+
+        <div className='form-floating my-1'>
+          <input
+            type='email'
+            className='form-control'
+            id='email'
+            name='email'
+            placeholder='name@example.com'
+            value=''
+            onChange={() => {}}
+          />
+          <label htmlFor='email'>Email address</label>
+        </div>
+
+        <div className='form-floating my-1'>
+          <select
+            className='form-select'
+            id='gender'
+            name='gender'
+            required=''
+            value=''
+            onChange={() => {}}
+          >
+            <option value=''>Choose...</option>
+            <option value='M'>Male</option>
+            <option value='F'>Female</option>
+          </select>
+          <label htmlFor='gender' className='form-label'>Gender</label>
+        </div>
+
+        <div className='form-floating my-1'>
+          <input
+            type='password'
+            className='form-control'
+            id='password'
+            name='password'
+            placeholder='Password'
+            value=''
+            onChange={() => {}}
+          />
+          <label htmlFor='password'>Password</label>
+        </div>
+
+        <div className='form-floating my-1'>
+          <input
+            type='password'
+            className='form-control'
+            id='password_confirm'
+            name='password_confirm'
+            placeholder='Repeat your Password'
+            value=''
+            onChange={() => {}}
+          />
+          <label htmlFor='password_confirm'>Repeat your Password</label>
+        </div>
+
+        <button className='my-3 w-100 btn btn-lg btn-primary' type='submit'>Sign up</button>
+        <p className='mt-5 mb-3 text-muted'>© 2017–2021</p>
+      </form>
+    </main>
   )
 }
 
