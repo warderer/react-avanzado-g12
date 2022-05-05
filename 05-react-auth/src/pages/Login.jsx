12cf2bc99f87a46fc62ea3518f5302b5e1439a2f
César Guerra <warderer@gmail.com>
2022-05-04 19:36:49 -0500

    💄 UI de Login

--- 05-react-auth/src/pages/Login.jsx
+++ 05-react-auth/src/pages/Login.jsx
@@ -1,6 +1,48 @@
+import '@/assets/css/form.css'
+import logo from '@/logo.svg'
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <main className='form-signin'>
+      <form onSubmit={() => {}}>
+        <img className='mb-4' src={logo} alt='' width='72' height='57' />
+        <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+
+        <div className='form-floating'>
+          <input
+            type='email'
+            className='form-control'
+            id='email'
+            name='email'
+            placeholder='name@example.com'
+            value=''
+            onChange={() => {}}
+          />
+          <label htmlFor='email'>Email address</label>
+        </div>
+
+        <div className='form-floating'>
+          <input
+            type='password'
+            className='form-control'
+            id='password'
+            name='password'
+            placeholder='Password'
+            value=''
+            onChange={() => {}}
+          />
+          <label htmlFor='password'>Password</label>
+        </div>
+
+        <div className='checkbox mb-3'>
+          <label>
+            <input type='checkbox' value='remember-me' /> Remember me
+          </label>
+        </div>
+        <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign in</button>
+        <p className='mt-5 mb-3 text-muted'>© 2017–2021</p>
+      </form>
+    </main>
   )
 }
 
