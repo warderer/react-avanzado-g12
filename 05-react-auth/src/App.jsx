2ee5ad2ca887356f1b7d5f33e7495d772ba18689
César Guerra <warderer@gmail.com>
2022-05-09 20:33:25 -0500

    ✨ Implementación del Proveedor del Contexto

--- 05-react-auth/src/App.jsx
+++ 05-react-auth/src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import RoutesIndex from '@/routes'
 import { Navbar } from '@/components/Navbar'
+import AuthContextProvider from '@/context/AuthContext'
 import './App.css'
 
 function App () {
   return (
-    <div>
-      <Router>
-        <Navbar />
-        <RoutesIndex />
-      </Router>
-    </div>
+    <>
+      <AuthContextProvider>
+        <Router>
+          <Navbar />
+          <RoutesIndex />
+        </Router>
+      </AuthContextProvider>
+    </>
   )
 }
 
